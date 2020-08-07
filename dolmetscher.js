c17abf7161d118c83174d8e50b239822d19300e9
= <nidhalbacc@gmail.com>
2020-08-07 23:10:41 +0200

    first functional example

---
+++ dolmetscher.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const axios = require('axios');
+const config = require('./config.json');
+
+console.log(config, typeof(config));
+
+
+async function translate(from, to, text) {
+
+    const params = {
+        sl: from,
+        hl: to,
+        q: text
+    };
+
+    try {
+      const response = await axios.get(config.host, {params: params});
+      const html = response.data;
+      const idx = html.search(config.pattern) + config.pattern.length;
+      const body = html.slice(idx);
+      const translated = body.substring(
+          0, body.indexOf('<')
+      );
+      
+      return translated;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
