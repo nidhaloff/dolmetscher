9325f51b5eb5de868ce3b76e7fdafb3e3b55dabb
= <nidhalbacc@gmail.com>
2020-08-07 23:10:47 +0200

    first functional example

--- dolmetscher.js
+++ dolmetscher.js
@@ -1,31 +1,26 @@
 'use strict'
 
-const axios = require('axios');
-const config = require('./config.json');
+const axios = require('axios')
+const config = require('./config.json')
 
-console.log(config, typeof(config));
-
+console.log(config, typeof config)
 
 async function translate(from, to, text) {
-
-    const params = {
-        sl: from,
-        hl: to,
-        q: text
-    };
-
-    try {
-      const response = await axios.get(config.host, {params: params});
-      const html = response.data;
-      const idx = html.search(config.pattern) + config.pattern.length;
-      const body = html.slice(idx);
-      const translated = body.substring(
-          0, body.indexOf('<')
-      );
-      
-      return translated;
-
-    } catch (error) {
-      console.log(error);
-    }
+  const params = {
+    sl: from,
+    hl: to,
+    q: text,
   }
+
+  try {
+    const response = await axios.get(config.host, { params })
+    const html = response.data
+    const idx = html.search(config.pattern) + config.pattern.length
+    const body = html.slice(idx)
+    const translated = body.substring(0, body.indexOf('<'))
+
+    return translated
+  } catch (error) {
+    console.log(error)
+  }
+}
