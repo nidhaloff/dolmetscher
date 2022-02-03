914e4da4519fc4c7ea557dd8b76c468cc9df0d74
Sina Elahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2022-02-03 18:39:05 +0330

    Update batchTrans.js

--- examples/googleTranslate/batchTrans.js
+++ examples/googleTranslate/batchTrans.js
@@ -2,11 +2,37 @@ const { GoogleTranslator } = require("../../src/dolmetscher");
 
 const google = new GoogleTranslator("en", "auto");
 
+async function alternativeBatchTranslation(texts) {
+  const result = []
+  
+  for(i = 0; i < texts.length; i++) {
+    await google
+    .translateBatch([texts[i]])
+    .then(res => result.push(res.pop()))
+    .catch(err => {
+      result.push('ERROR: ' + err) 
+    })
+    if(i == texts.length - 1) {
+      console.log(result)
+    }
+  }
+  
+  // await google
+  //   .translateBatch([texts[i]])
+  //   .then(res => process.stdout.write(' \'' + res.pop() + '\' '))
+  //   .catch(err => {
+  //     process.stdout.write(' \'' + err + + '\' ') 
+  //   })
+}
 function batchTranslation() {
-  const texts = ["bonsoir le monde", "Hallo Welt", "guten morgen"];
+  const texts = ["", "Hallo Welt", "guten morgen"];
   google
     .translateBatch(texts)
-    .then(res => console.log("batch translation: ", res));
+    .then(res => console.log("batch translation: ", res))
+    .catch(err => {
+      alternativeBatchTranslation(texts)
+    })
+    // .catch(err => console.log('an error has been occured! ', err));
 }
 
 batchTranslation();
